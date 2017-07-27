1c51db17da7ed5472d21d5482d40caa45a370ead
tenkiss258 <837442643@qq.com>
2017-07-27 16:45:53 +0800

    '修改了一些bug‘

--- zhycw/js/header.js
+++ zhycw/js/header.js
@@ -11,15 +11,28 @@
             $(".nav_btn i:first-child").attr("class","");
             $(".nav_btn i:last-child").attr("class","");
             $(".nav_son_box").css("display","none");
-
             flag=true;
         }
-
-    })
+    });
     var nav_box_li=$(".nav_box li");
-    nav_box_li.hover(function(){
+    nav_box_li.click(function(){
         $(".nav_son_box").css("display","block");
-    },function(){
+        $(".nav_btn i:first-child").attr("class","active");
+        $(".nav_btn i:last-child").attr("class","active");
+        flag=false;
+    });
+    var nav_two_li=$(".nav_two li");
+    nav_two_li.click(function(){
         $(".nav_son_box").css("display","none");
+        $(".nav_btn i:first-child").attr("class","");
+        $(".nav_btn i:last-child").attr("class","");
+        flag=true;
+    });
+    var nav_two_qx=$(".nav_two_qx");
+    nav_two_qx.click(function(){
+        $(".nav_son_box").css("display","none");
+        $(".nav_btn i:first-child").attr("class","");
+        $(".nav_btn i:last-child").attr("class","");
+        flag=true;
     })
-})()
+})();
